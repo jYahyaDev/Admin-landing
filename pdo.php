a4f0c139d7a3bc7fdce66393879544c6a74ffade
jYahyaDev <62561346+jYahyaDev@users.noreply.github.com>
2021-02-19 09:43:40 +0100

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -3,7 +3,7 @@
   const DB_HOST    = "localhost";
   const DB_NAME    = "landing";
   const DB_LOGIN   = "jyahya";
-  const DB_PASS    = "Azerty30900!";
+  const DB_PASS    = "";
 
   const DB_DRIVER  = "mysql";
   const DB_CHARSET = "utf8mb4";
@@ -17,27 +17,3 @@
 
 
 
-/* 
-   try{
-
-       $pdo = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET, DB_LOGIN, DB_PASS, DB_OPTIONS);
-
-        $pseudo = "capucine";
-        $pass = "landing";
-        $pass_hache = password_hash($pass, PASSWORD_DEFAULT);
-
-        $requete = 'INSERT INTO `login`(`pseudo`, `password`) VALUES (:pseudo,:pass)';
-        $stmt = $pdo->prepare($requete);
-        $stmt->execute(array(
-            ":pseudo" => $pseudo,
-            ":pass" => $pass_hache
-        ));
-
-
-   } catch(PDOException $e) {
-
-    // en cas d'erreur, on récup et on affiche, grâce à notre try/catch
-    exit("❌🙀💀 OOPS :\n" . $e->getMessage());
-
-  }
- */
